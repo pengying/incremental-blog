88ca5f4ff4d4174329c1bfd9cc901bafe31ec6a2
Peng Ying <peng@synctera.com>
2023-08-04 14:22:50 -0700

    getting initial skeleton

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import remarkGfm from "remark-gfm";
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({
+  options: {
+    extension: /\.mdx?$/,
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
+};
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
