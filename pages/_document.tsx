afbd2b5d4fc777d4cf858921ca4ca1abd71e60a9
Peng Ying <peng@synctera.com>
2023-08-18 15:37:53 -0700

    adding analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import Document, {
   Html,
   Head,
@@ -6,12 +6,13 @@ import Document, {
   NextScript,
   DocumentProps,
   DocumentContext,
-} from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import { AppType } from 'next/app';
-import theme from '../styles/theme/mui-theme';
-import createEmotionCache from '../lib/createEmotionCache';
-import { MyAppProps } from './_app';
+} from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import { AppType } from "next/app";
+import theme from "../styles/theme/mui-theme";
+import createEmotionCache from "../lib/createEmotionCache";
+import { MyAppProps } from "./_app";
+import Script from "next/script";
 
 interface MyDocumentProps extends DocumentProps {
   emotionStyleTags: JSX.Element[];
@@ -26,6 +27,13 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="emotion-insertion-point" content="" />
         {emotionStyleTags}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-QKCWGT5WXP" />
+        <Script id="g-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-QKCWGT5WXP');`}
+        </Script>
       </Head>
       <body>
         <Main />
@@ -69,7 +77,9 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App: React.ComponentType<React.ComponentProps<AppType> & MyAppProps>) =>
+      enhanceApp: (
+        App: React.ComponentType<React.ComponentProps<AppType> & MyAppProps>
+      ) =>
         function EnhanceApp(props) {
           return <App emotionCache={cache} {...props} />;
         },
@@ -79,9 +89,9 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
   // This is important. It prevents Emotion to render invalid HTML.
   // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
   const emotionStyles = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = emotionStyles.styles.map((style:any) => (
+  const emotionStyleTags = emotionStyles.styles.map((style: any) => (
     <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
       key={style.key}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: style.css }}
